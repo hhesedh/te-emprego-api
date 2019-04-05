87e80f64abadee8af25d54025f5a907dbf8a5178
fabio <fabiodeoliveira07@gmail.com>
2019-04-05 13:23:47 -0300

    hello world with express + eslint

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('hello, world');
+});
+
+app.listen(4000, (err) => {
+  if (err) {
+    return console.log('erro');
+  }
+
+  console.log('iniciou em http://localhost:4000');
+});
