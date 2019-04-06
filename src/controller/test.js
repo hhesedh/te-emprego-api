2b59d9be2c9be00db647936e521467bdffe791cf
fabio <fabiodeoliveira07@gmail.com>
2019-04-06 10:21:49 -0300

    Definindo a arquitetura do projeto

---
+++ src/controller/test.js
@@ -0,0 +1,7 @@
+const TestController = {
+  helloWorld(req, res) {
+    res.send('hello, gatinho');
+  },
+};
+
+module.exports = TestController;
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const TestController = require('@controller/test');
+
+router.get('/', TestController.helloWorld);
+
+module.exports = router;
